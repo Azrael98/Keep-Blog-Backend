a9decd9dbf6b043f75e66d0ce85734d47c5a6ae7
harsh <harshjais30@gmail.com>
2022-11-17 01:56:59 +0530

    added netlify origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:5500',
-    'http://127.0.0.1:5500'
+    'http://127.0.0.1:5500',
+    'https://peppy-longma-906ac0.netlify.app'
 ]
 
 export default allowedOrigins;
