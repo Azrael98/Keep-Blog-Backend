d882c50657d220fb707e53cd9e3e34f0f13e3b1b
harsh <harshjais30@gmail.com>
2022-11-17 19:28:12 +0530

    cors update

--- config/allowedOrigins.js
+++
@@ -1,8 +0,0 @@
-const allowedOrigins = [
-    'http://localhost:3000',
-    'http://localhost:5500',
-    'http://127.0.0.1:5500',
-    'https://peppy-longma-906ac0.netlify.app'
-]
-
-export default allowedOrigins;
