570920f424cb02394a64e424ed1910a16fc03ebd
harsh <harshjais30@gmail.com>
2022-12-29 16:58:51 +0530

    port fix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import dotenv from "dotenv";
 
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||3000;
 const app = express();
 
 app.use(cors({ credentials: true }));
