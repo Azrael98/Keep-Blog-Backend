16d45f92846d079784f6cc0415c2fb1a35456306
harsh <harshjais30@gmail.com>
2022-12-29 16:54:04 +0530

    mongodb

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use("/api/posts", postRoutes);
 
 app.use("/api/auth", authRoutes);
 
-app.use("/api/users", userRoutes);
+app.use("/api/users", userRoutes)
 
 mongoose
   .connect(process.env.MONGO_URL, {
